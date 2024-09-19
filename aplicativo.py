2dea0cc5b429c80a678e5a1e9e80d90ce268b617
vladimirmega <vladimir.gomes@megamamute.com.br>
2024-09-18 21:28:15 -0300

    Estrutura do projeto

---
+++ aplicativo.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+if __name__=="__main__":
+    spark = SparkSession.builder.appName("Exemplo").getOrCreate()
+    arqschema = "id INT, nome STRING, status STRING, cidade STRING, vendas INT, data STRING"
+    despachantes = spark.read.csv("/home/estudante/download/despachantes.csv", header=False, schema=arqschema)
+    calculo = despachantes.select("data").groupBy(year("data")).count()
+    calculo.write.format("console").save()
+    spark.stop()
+    
